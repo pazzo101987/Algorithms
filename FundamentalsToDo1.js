a09d773b04ee46d39ff8fd46fb5f24835a41b452
Hamza87 <hamza.dridi1987@gmail.com>
2023-09-22 12:02:05 +0100

    Added a new file

---
+++ FundamentalsToDo1.js
@@ -0,0 +1,20 @@
+// 1-Setting and Swapping
+
+let myNumber = 42;
+let myName = "Hamza";
+
+// Swapping values using a temporary variable
+let temp = myNumber;
+myNumber = myName;
+myName = temp;
+
+console.log("myNumber:", myNumber);
+console.log("myName:", myName);
+
+// 2- Print -52 to 1066
+
+for (var i = -52; i <= 1000; i++) {
+    console.log(i);
+}
+
+console.log("Done!");
